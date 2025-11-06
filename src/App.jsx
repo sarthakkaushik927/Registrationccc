fd9ce0588ea3a1cdad21b055538e5e3628423234
sarthakkaushik927 <sarthakkaushik927@gmail.com>
2025-11-06 22:16:23 +0530

    error

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Register from './components/Register'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    < Register/>
+    </>
+  )
+}
+
+export default App
